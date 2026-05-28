c70d5d3511687b1961684152fd0ad48936161c69
NikhilSai <nikhilsaimandava26@gmail.com>
2026-05-28 19:00:26 +0530

    modified jenkins commands

--- downloads/jenkins.sh
+++ downloads/jenkins.sh
@@ -2,7 +2,7 @@ yum install java-21-amazon-corretto -y # step 1: install java
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
   https://pkg.jenkins.io/rpm-stable/jenkins.repo # step 2: add repo
-sudo yum upgrade# step 3: upgrade packages
+sudo yum upgrade # step 3: upgrade packages
 
 sudo yum install jenkins # step 4: install jenkins
 
